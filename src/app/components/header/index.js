893e9111ead9001d48330008eb7c019572c1d014
locnguyenhuu0024 <18050078@student.bdu.edu.vn>
2022-08-24 10:26:34 +0700

    done feature/build-header-1

--- src/app/components/header/index.js
+++ src/app/components/header/index.js
@@ -1,9 +1,22 @@
-import { Layout } from 'antd';
+import { Layout, Space, Typography } from 'antd';
 
+const style = {
+    textAlign: 'center'
+}
 function Headerf(){
     return (
-        <Layout.Header>
-
+        <Layout.Header
+            style={style}
+        >
+            <Space
+                size={'middle'}
+            >
+                <Typography.Title
+                    style={{color: 'white'}}
+                >
+                    Quản lý danh sách sinh viên
+                </Typography.Title>
+            </Space>
         </Layout.Header>
     )
 }
